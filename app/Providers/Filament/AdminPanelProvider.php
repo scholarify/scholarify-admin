75150b0460dbb7a4b0d902804032b1bb54a6ee9d
Franck Elysee <franckelysee671@gmail.com>
2025-03-24 11:57:34 +0100

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Login;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login([AuthenticatedSessionController::class, 'create'])
             ->colors([
                 'primary' => Color::Amber,
             ])
